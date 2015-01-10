e74e0454a334b1b46c02ae20b3961b57e2b27e0d
GoldenGnu <nkr@niklaskr.dk>
2015-01-10 21:05:05 +0000

    Making ready to release 1.2.0

--- src/main/java/net/nikr/update/Program.java
+++ src/main/java/net/nikr/update/Program.java
@@ -33,7 +33,7 @@ import net.nikr.update.io.ListGetter;
 
 public class Program {
 
-	public static final String PROGRAM_VERSION = "1.1.0";
+	public static final String PROGRAM_VERSION = "1.2.0";
 
 	private boolean test;
 	
