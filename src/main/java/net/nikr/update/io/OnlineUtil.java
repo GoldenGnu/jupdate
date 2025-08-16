6bc0bb2c853433f0b8e6249e8d941bb86fd65a4c
GoldenGnu <nkr@niklaskr.dk>
2025-08-16 22:29:39 +0200

    Code: 1 request/second >_<

--- src/main/java/net/nikr/update/io/OnlineUtil.java
+++ src/main/java/net/nikr/update/io/OnlineUtil.java
@@ -30,8 +30,6 @@ import net.nikr.update.update.OnlineError;
 
 
 public class OnlineUtil {
-
-	public static Long pause = null;
 	
 	public static boolean downloadFile(final String from, final File to, final File check, boolean sub) throws OnlineError, LocalError {
 		DataGetter getter = new DataGetter();
@@ -69,18 +67,15 @@ public class OnlineUtil {
 	}
 
 	public static void pause() {
-		if (pause != null) {
-			try {
-				Thread.sleep(pause);
-			} catch (InterruptedException ex) {
-				System.out.println(ex.getMessage());
-			}
+		try {
+			Thread.sleep(1000L);
+		} catch (InterruptedException ex) {
+			System.out.println(ex.getMessage());
 		}
 	}
 
 	public static void breaksOn() {
 		try {
-			pause = 1000L;
 			Thread.sleep(10000L);
 		} catch (InterruptedException ex) {
 			System.out.println(ex.getMessage());
